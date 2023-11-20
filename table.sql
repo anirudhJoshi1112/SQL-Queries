b7a60d6bcd195d2245a0e922351b391528d712e1
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-11-20 06:51:24 +0530

    Basic creation of a table and inserting values in it and show the table

---
+++ table.sql
@@ -0,0 +1,6 @@
+create table StudentList(stdid int(10) primary key,
+                      stdname varchar(20),
+                      percentagee varchar(20),
+                      grade varchar(20));
+insert into StudentList values(01,Ashutosh Upadhyay,80,A);
+Select * from StudentList;   
