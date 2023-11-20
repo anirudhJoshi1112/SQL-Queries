9321bf68faf85b1a46382e0c52209c282366f61c
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-11-20 06:59:07 +0530

    creation if a new table using the data of existing table

---
+++ tableA.sql
@@ -0,0 +1,8 @@
+create table StudentList2 as select stdid, stdname from StudentList;
+
+
+for example->
+  
+stdid               stdname
+01                  Ashutosh Upadhyay
+02                  swayambhu ravindra
