ad50afb502e08a98e41af2a364f60385abe40aa3
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-11-20 07:10:37 +0530

    LIMIT -> used to get limited data from table

---
+++ tableC.sql
@@ -0,0 +1,6 @@
+create table StudentDATA as select * from StudentList LIMIT 3;
+
+stdid    stdname    percentage    grade
+01       Ashutosh    80            A
+02       swayambhu   90            AA
+03       rudrama     91            AAA
